acab13be5900c362d9ddb013f526029a571b693f
gmifat <gmiden.fatma@gmail.com>
2021-05-16 04:15:01 +0200

    add form class session

--- class_session.py
+++ class_session.py
@@ -9,17 +9,19 @@ from base import school_db
 @app.route('/class_sessions/<class_id>', methods=['GET'])
 def get_class_sessions(class_id):
     school_cursor = school_db.cursor()
-    school_cursor.execute("select class.class_id, class.class_name,class_subject.subject_id, subject.subject_name"
+    school_cursor.execute("select class.class_id, class.class_name,class_subject.subject_id, subject.subject_name,"
+                          " class_subject.class_subject_id"
                           " from class left join class_subject on class.class_id = class_subject.class_id"
                           " left join subject on class_subject.subject_id = subject.subject_id"
                           " where class.class_id = %(cl_id)s",
                           {'cl_id': class_id})
     class_info = school_cursor.fetchall()
-    school_cursor.execute("select tc.teacher_id, tc.first_name, tc.last_name, tc.grade, tc.email, tc.address,"
-                          " tc.phone, sb.subject_id, sb.subject_name from teacher tc"
+    school_cursor.execute("select speciality.speciality_id,"
+                          " tc.teacher_id, tc.first_name, tc.last_name, sb.subject_id, sb.subject_name"
+                          " from teacher tc"
                           " join speciality on speciality.teacher_id = tc.teacher_id"
                           " join subject sb on speciality.subject_id = sb.subject_id"
-                          " order by sb.subject_name")
+                          " order by tc.teacher_id")
     teacher_info = school_cursor.fetchall()
     school_cursor.execute("select classroom_id, room_number from classroom order by room_number")
     classrooms = school_cursor.fetchall()
@@ -46,3 +48,26 @@ def get_class_sessions(class_id):
     school_cursor.close()
     return render_template('class_session/class_session.html', class_info=class_info, teacher_info=teacher_info,
                            classrooms=classrooms, class_sessions=grp_class_sessions)
+
+
+@app.route('/class_sessions', methods=['POST'])
+def add_class_sessions():
+    school_cursor = school_db.cursor()
+    school_cursor.execute("insert into class_session (speciality_id, class_subject_id, classroom_id,"
+                          " day, start_time, end_time, virtual)"
+                          " values (%(speciality_id)s, %(class_subject_id)s, %(classroom_id)s,"
+                          " %(day)s, %(start_time)s, %(end_time)s), %(virtual)s)",
+                          {
+                            'speciality_id': request.form['speciality_id'],
+                            'class_subject_id': request.form['class_subject_id'],
+                            'classroom_id': request.form['classroom_id'],
+                            'day': request.form['day'],
+                            'start_time': request.form['start_time'],
+                            'end_time': request.form['end_time'],
+                            'virtual': request.form['virtual']
+                          })
+    school_db.commit()
+    school_cursor.close()
+    return redirect('/admins')
+
+    return "In Progress"
